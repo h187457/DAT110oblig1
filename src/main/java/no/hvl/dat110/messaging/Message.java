67f91f1314af0c14b92df8765ed8dc97550223a5
h187457 <187457@stud.hvl.no>
2026-02-13 15:44:49 +0100

    message filen

--- src/main/java/no/hvl/dat110/messaging/Message.java
+++ src/main/java/no/hvl/dat110/messaging/Message.java
@@ -4,19 +4,21 @@ import no.hvl.dat110.TODO;
 
 public class Message {
 
-	// the up to 127 bytes of data (payload) that a message can hold
 	private byte[] data;
 
-	// construction a Message with the data provided
-	public Message(byte[] data) {
-		
-		// TODO - START
-		
-		if (true)
-			throw new UnsupportedOperationException(TODO.constructor("Message"));
-			
-		// TODO - END
-	}
+    public Message(byte[] data) {
+
+        if (data == null) {
+            throw new IllegalArgumentException("Data cannot be null");
+        }
+
+        if (data.length > MessageUtils.SEGMENTSIZE) {
+            throw new IllegalArgumentException("Data too long");
+        }
+
+        this.data = data;
+    }
+
 
 	public byte[] getData() {
 		return this.data; 
